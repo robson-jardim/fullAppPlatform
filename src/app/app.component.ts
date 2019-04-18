8636914810242d02753e5c47b1f2b8af37a05831
Robson Jardim <robson.jardim@appstofly.com>
2019-04-18 02:36:20 -0300

    Add PWA Beta Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+//import { Platform } from '@ionic/angular'; //Remove if it's only a PWA
+//import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+//import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 @Component({
   selector: 'app-root',
@@ -23,17 +23,17 @@ export class AppComponent {
   ];
 
   constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    //private platform: Platform,
+    //private splashScreen: SplashScreen,
+    //private statusBar: StatusBar
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
+    //this.platform.ready().then(() => {
+    //  this.statusBar.styleDefault();
+    // this.splashScreen.hide();
+    //});
   }
 }
